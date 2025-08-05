39a263586ea366025f09092bde91c5b97c072657
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-08-05 21:30:10 +0100

    git cloned

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Notes from './pages/Notes';
 import Create from './pages/Create';
+import { purple } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#fefefe'
+      },
+      secondary: purple
+    },
+    typography: {
+      fontFamily: 'Quicksand',
+      fontWeightLight: 400,
+      fontWeightRegular: 500,
+      fontWeightMedium: 600,
+      fontWeightBold: 700,
+    }
+  });
   return (
+    <ThemeProvider theme={theme}>
     <Router>
       <Routes>
         <Route path="/" element={<Notes />} />
         <Route path="/create" element={<Create />} />
       </Routes>
     </Router>
+  </ThemeProvider>
   );
 }
 
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { useTheme } from '@mui/material/styles';
 
 export default function Create() {
+  const themes = useTheme()
   return (
-    <div>
-      Create page
-    </div>
+    <Container size="sm">
+      <Typography
+        variant="h6" 
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        onClick={() => console.log('you clicked me')}
+        type="submit" 
+        color="secondary" 
+        variant="contained"
+        endIcon={<ChevronRightIcon />}>
+        Submit
+      </Button>
+    </Container>
   )
 }
-
